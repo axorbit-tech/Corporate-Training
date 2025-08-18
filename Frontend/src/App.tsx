2c329ac07226493abd33af1fcd2f505c52a9ae85
rafirafi03 <ahamedrafirafi03@gmail.com>
2025-08-18 17:10:09 +0530

    git add app.tsx

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -30,6 +30,7 @@ import AdminEditServicePage from "./pages/admin/service/EditService";
 import AdminLoginPage from "./pages/admin/auth/AdminLogin";
 import AdminEditBlogPage from "./pages/admin/blog/EditBlog";
 import AdminEditEventPage from "./pages/admin/event/EditEvent";
+import AdminClientListingPage from "./pages/admin/client/ClientListing";
 
 function App() {
   return (
@@ -82,6 +83,9 @@ function App() {
             path="/admin/edit-service/:id"
             element={<AdminEditServicePage />}
           />
+
+
+          <Route path="/admin/clients" element={<AdminClientListingPage />} />
 
           <Route path="/admin/login" element={<AdminLoginPage />} />
 
